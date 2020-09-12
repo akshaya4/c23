e106b7bf658dcad4f320fee200d48e931c70c396
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-12 09:11:11 +0530

    Update paper.js

--- paper.js
+++ paper.js
@@ -1,10 +1,10 @@
 class Paper {
     constructor(x, y, r) {
         var options={
-            isStatic: false,
-            restitution: 0.3,
-            friction: 0.5,
-            density: 1.2
+            'isStatic': true,
+            'restitution': 0.3,
+            'friction': 0.5,
+            'density': 1.2
         }
         this.x = x;
         this.y = y;
