5a16546739a180065343b6ee3dd0a5fc3c43bc80
akshaya4 <70489515+akshaya4@users.noreply.github.com>
2020-09-12 14:55:09 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,11 +5,7 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var paper, ground;
-
-function preload()
-{
-	
-}
+var dustbinObj,world;
 
 function setup() {
 	createCanvas(800, 700);
@@ -20,8 +16,18 @@ function setup() {
 
 	//Create the Bodies Here.
 	paper = new Paper(200,200,20)
-	
+	dustbinObj=new dustbin(1200,650);
 	ground = new Ground(400, 600, 800, 10);
+	
+	var render = Render.create({
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
 
 	Engine.run(engine);
   
@@ -30,9 +36,10 @@ function setup() {
 
 function draw() {
   
+  rectMode(CENTER);
 	background(0);
 
-
+dustbinObj.display();
 paper.display();
 ground.display();
 
@@ -44,6 +51,6 @@ ground.display();
 function keyPressed() {
 	if (keyCode === UP_ARROW)
 
-	Body.applyForce(paper.body, paper.body.position, {x: 85, y:-85})
+	Matter.Body.applyForce(paper.body, paper.body.position, {x: 85, y:-85})
 }
 
